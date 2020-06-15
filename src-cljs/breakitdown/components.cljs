53cbf97bb7da75ad437305c6bbf5bbf41766ffce
Govindarajan <govindnagarajan@outlook.com>
2020-06-15 10:58:01 +0200

    Refactor: Move `search-form` from one core to components namespace

--- src-cljs/breakitdown/components.cljs
+++ src-cljs/breakitdown/components.cljs
@@ -19,3 +19,40 @@
 (defn simple-component [prop]
   [:div
    [:p "I am a component and I have been reloaded " (str  prop) " time"]])
+
+(defn search-form
+  []
+  (let [
+        set-in-s (fn [event k] (swap! app-state assoc k (-> event .-target .-value)))]
+    (fn render []
+      [:div
+       [:form {:on-submit (fn [e]
+                            (println "button clicked")
+                            (.preventDefault e)
+                            (form-search @app-state app-state))}
+
+        [:div
+         [:p "pin"]]
+        [:div
+         [:input {:type :text
+                  :name :pin
+                  :on-change #(set-in-s % :pin)}]
+         [:p "or"]]
+        [:divtext
+         [:input {:type :text
+                  :name :location
+                  :on-change #(set-in-s % :location)}]
+         [:br]]
+        [:div
+         [:p "And I am searching for"]
+         [:input {:type :text
+                  :name :need
+                  :on-change #(set-in-s % :need)}]]
+        [:div
+         [:button.submitbutton "Submit"]]]
+       [:button.clearbutton {:on-click (fn [e]
+                                         (.preventDefault e)
+                                         (swap! app-state dissoc :results))}
+        "clear"]
+       [:div
+        [show-results app-state]]])))
